764f221b213c223936a007ff037fe1d70667607c
Jane Greene <janegreenecasa@gmail.com>
2020-09-10 18:26:33 -0600

    Created user model migration and factory needs more tests User controller with show method
    Co-authored-by: Dorian <60492885+sciencefixion@users.noreply.github.com>
    Co-authored-by: Kathy Bui 
    <61847658+Kathybui732@users.noreply.github.com>
    Co-authored-by: Dan Sehl <62079009+dtsehl@users.noreply.github.com>

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+  validates :name, presence: true
+  validates :uid, uniqueness: true, presence: true
+
+  def self.from_omniauth(params)
+    user = find_or_create_by(uid: params[:uid])
+    user.name = params[:info][:name]
+    user.email = params[:info][:email]
+    user.avatar = params[:info][:image]
+    user.access_token = params[:credentials][:token]
+    user.refresh_token = params[:credentials][:refresh_token]
+    user.save!
+    user
+  end
+end
