5fc04a23b8864927953a76971b29d196bc9cbf55
Dan Sehl <dan.sehl@gmail.com>
2020-09-13 15:35:03 -0600

    Add the image show route and search show route

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
   get '/logout', to: 'sessions#destroy'
   get '/dashboard', to: 'users#show'
   get '/search', to: 'search#index'
+  get '/search/:id', to: 'search#show'
+  post '/image', to: 'images#show'
 end
