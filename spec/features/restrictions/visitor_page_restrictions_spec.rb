4f49170dfa57d06a2865d3dd8a92fa07b00ed156
Kathy Bui <kathy.bui1992@gmail.com>
2020-09-14 12:49:19 -0600

    Create spec to test for visitor restrcitions on site Expect visitors to get a 404 response when trying to visit /search or /dashboard

---
+++ spec/features/restrictions/visitor_page_restrictions_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe 'Restrcitions' do
+  describe 'As a visito' do
+    it "renders 404 when visiting any page other than welcome page" do
+      visit '/dashboard'
+      expect(page).to have_content("The page you were looking for doesn't exist.")
+
+      visit '/search'
+      expect(page).to have_content("The page you were looking for doesn't exist.")
+    end
+  end
+end
