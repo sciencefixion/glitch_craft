c5b698022bc474f617c9e9fc14610ec058180575
Jane Greene <janegreenecasa@gmail.com>
2020-09-14 16:41:09 -0600

    Removed error methods as rails does not need them
    Co-authored-by: Dorian <60492885+sciencefixion@users.noreply.github.com>
    Co-authored-by: Kathy Bui <61847658+Kathybui732@users.noreply.github.com>
    Co-authored-by: Dan Sehl <62079009+dtsehl@users.noreply.github.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,13 +5,6 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:id]) if session[:id]
   end
 
-  def server_errors
-    render status: :internal_server_error
-  end
-
-  def search_errors
-    render status: :bad_request
-  end
   private
 
   def require_user
