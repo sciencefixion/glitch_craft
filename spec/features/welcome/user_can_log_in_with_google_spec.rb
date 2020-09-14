e8524444fcaa9e667946c6d3bbe65b9381786ca3
Jane Greene <janegreenecasa@gmail.com>
2020-09-14 11:16:41 -0600

    Added logout test to existing test for login
    Co-authored-by: Dorian <60492885+sciencefixion@users.noreply.github.com>
    Co-authored-by: Kathy Bui <61847658+Kathybui732@users.noreply.github.com>
    Co-authored-by: Dan Sehl <62079009+dtsehl@users.noreply.github.com>

--- spec/features/welcome/user_can_log_in_with_google_spec.rb
+++ spec/features/welcome/user_can_log_in_with_google_spec.rb
@@ -7,4 +7,15 @@ RSpec.feature "user logs in" do
   click_link "Login with Google to Start Glitching"
   expect(page).to have_content("Sample Name")
  end
+ it "and user logs out" do
+   mock_user
+   visit root_path
+
+   click_link "Login with Google to Start Glitching"
+   expect(page).to have_content("Sample Name")
+   click_on 'Logout'
+
+   expect(current_path).to eq(root_path)
+   expect(page).to_not have_content("Sample Name")
+ end
 end
