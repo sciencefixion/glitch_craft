b12a1998214a6e8274144dbe357f0309765f1b58
Kathy Bui <kathy.bui1992@gmail.com>
2020-09-16 16:54:24 -0600

    Update flash messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   def create
     user = User.from_omniauth(request.env['omniauth.auth'])
     session[:id] = user.id
-    flash[:success] = "Welcome #{user.name}"
+    flash[:success] = "Welcome, #{user.name}!"
 
     redirect_to '/dashboard'
   end
