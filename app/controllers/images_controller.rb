c0efcc83f30153a34957e944d32f92f72fb228bb
Dan Sehl <dan.sehl@gmail.com>
2020-09-16 19:07:32 -0600

    Add logic to make a request to AWS in Image controller create action

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,11 +1,18 @@
+require 'aws-sdk-s3'
+
 class ImagesController < ApplicationController
   before_action :require_user
   include Glitchable
   def create
+    credentials = Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'])
+    connection = Aws::S3::Client.new(region: 'us-east-1', credentials: credentials)
     image = current_user.api_images.create!(image_params)
     download = Down.download(image.url)
     image.original.attach(io: download, filename: 'image.jpg')
     basic_glitch(image)
+    response = File.open('lib/assets/glitched/new_image.png', 'rb') do |file|
+      connection.put_object(bucket: 'glitch-craft-images', key: 'image-key', body: file)
+    end
     redirect_to search_show_path(image)
   end
 
